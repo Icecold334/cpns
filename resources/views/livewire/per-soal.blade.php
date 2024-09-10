14380983f32b9d54c4ad511de14163d00173c8fc
Icecold334 <fauzanimam334@gmail.com>
2024-09-10 12:34:47 +0700

    gaada kemajuan

--- resources/views/livewire/per-soal.blade.php
+++ resources/views/livewire/per-soal.blade.php
@@ -1,3 +1,55 @@
 <div>
-    @dump($soals)
+    <div class="container mt-4">
+        <div class="card shadow">
+            <div class="card-header text-center">
+                <h3>Soal Nomor 1</h3>
+            </div>
+            <div class="card-body">
+                <p class="question-text font-weight-bold">soal</p>
+
+                <form>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
+                        <label class="form-check-label" for="answer1">
+                            a
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
+                        <label class="form-check-label" for="answer1">
+                            a
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
+                        <label class="form-check-label" for="answer1">
+                            a
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
+                        <label class="form-check-label" for="answer1">
+                            a
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
+                        <label class="form-check-label" for="answer1">
+                            a
+                        </label>
+                    </div>
+                </form>
+
+                <div class="mt-4">
+                    <p>Sisa waktu untuk soal ini: <span id="timeLeft" class="badge bg-primary">5</span> detik</p>
+                </div>
+            </div>
+
+            <div class="card-footer text-center">
+                <button class="btn btn-info" disabled>Soal akan otomatis berganti</button>
+            </div>
+        </div>
+    </div>
+
+
 </div>
